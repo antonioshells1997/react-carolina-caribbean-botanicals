f25f84b06ac3daab3b97a4694ee47f28ca597f20
antonioshells1997 <57854533+antonioshells1997@users.noreply.github.com>
2019-12-08 10:32:02 -0500

    intial commit

---
+++ src/data.js
@@ -0,0 +1,115 @@
+/*
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Holy Basil - Powder (1oz)",
+    img: "img/product-1.1.1.png",
+    price: 10,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'In the "Charaka Samhita" the consummate text on Ayurveda written around 1000 BC, Holy Basil is described as “the incomparable one” and is considered to be an incarnation of Tulasi or Vrindavani, a consort of Lord Vishnu. It is considered a symbol of fidelity and helpful in attaining spiritual enlightenment. It has been used to support a healthy response to stress, maintain blood sugar levels within a normal range, promote longevity, nourish the mind and elevate the spirit. Holy Basil (Ocimum sanctum) exhibits impressive adaptogenic activity. Helping the body adapt to stresses of various kinds, this plant has a normalizing, broad-spectrum action that maintain homeostasis.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Sage - Powder (1oz)",
+    img: "img/product-2.1.1.png",
+    price: 16,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'The genus, Salvia is the largest in the mint (Labitae), family with over 800 species represented. Salvia derives from the Latin salvere ("to save"), referring to the long-held belief in the herb\'\s healing properties. Pliny the Elder was the first author known to describe a plant called "Salvia" by the Romans. It has ben used widely as a culinary herb and traditional plant for a long list of functions. The plant has trichomes or hair like structures on the undersides of the leaves that when broken release stores of volatile oils which impart characteristic flavor and properties. Sage was officially listed in the United States Pharmacopoeia from 1840 to 1900.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Moringa - Powder (1oz)",
+    img: "img/product-3.1.1.png",
+    price: 8,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'Moringa is native to parts of Africa (Moringa stenopetala) and Asia (Moringa oleifera), and is entirely edible. It may actually be the key to ending malnutrition in many parts of the world. Biochemists, natural scientists, and healthcare workers are busy substantiating ancient claims of Moringa’s effectiveness in treating more than 300 conditions. Research has led to extensive documentation of Moringa\'\s astonishing range of natural vitamins, minerals, and other nutrients. The leaves are the most nutritious part of the plant, being a significant source of B vitamins, vitamin C, vitamin A and beta-carotene, vitamin K, manganese and protein, among other essential nutrients. Many of the nutrients increase in the dried powder form of the pant, as opposed to fresh leaves.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Neem Leaf - Powder (1oz)",
+    img: "img/product-4.1.1.png",
+    price: 18,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'The neem plant is a fast-growing and long-lasting broad-leaved evergreen tree. It is native to the Indian subcontinent, and from there it has been widely introduced to the rest of the world. Today it is present in the tropical and subtropical zones of Africa, the Americas, Asia, Australia and the South Pacific Islands. Readily available year-round, neem leaves boost the respiratory system, improve digestion, and strengthen the immune system. They also support the liver by getting rid of unwanted toxins in the blood. Neem leaves boiled in water help in clearing acne, scars, pigmentation and blackheads. Additionally, they also leave the skin soft and supple. Neem leaves are dried and burnt to keep away mosquitoes. They also act as a natural pesticide. Dried leaves protect rice kept in containers and clothes kept in drawers from insects. Drinking water infused with neem leaves controls high blood sugar. The leaves help to dilate blood vessels impeding blood coagulation, decrease elevated heart rates and relax erratic heartbeats. This is due to the presence of antihistamine compounds in them, and as a result, neem leaves keep a check on high blood pressure levels.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Tamarind - Powder (1oz)",
+    img: "img/product-5.1.1.png",
+    price: 24,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'The tamarind tree is a kind of tree that also produces a pod-like fruit (often referred to by the same name) that happens to be a legume. This versatile and delicious fruit also contains a myriad of health benefits. While the original home of tamarind is Africa, countries like India and Pakistan now primarily export it. Tamarind, after all, plays a HUGE role in making Indian/South Asian cuisine utterly delicious! As previously mentioned, tamarind has a plethora of health benefits. Some of its studied effects include lowering cholesterol and preventing the absorption of fluoride in humans. Fluoride has been shown to have negative effects on cognitive abilities, but the use of tamarind can help the body expel this mineral. Tamarind has also played an important role in traditional medicine. Traditional medicine practitioners and healers around the world have used the beverage form of tamarind to commonly treat diarrhea, constipation, fever and peptic ulcers. They also use the bark and leaves to promote wound healing by creating a paste from the pulp as a makeshift band-aid. The polyphenols in tamarind have antioxidant and anti-inflammatory properties, which are important for protecting the epidermis from constant exposure from free radicals. In addition, these polyphenols can protect against diseases such as heart disease, cancer and diabetes. The seed extract may also help lower blood sugar, while the pulp extract may help you lose body weight and reverse fatty liver disease.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Mint - Powder (1oz)",
+    img: "img/product-6.1.1.png",
+    price: 17,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+    'We flavor our toothpastes, chewing gums, and breath fresheners with Mint’s fresh and cooling fragrance. There are over 600 varieties of Mint all have a square stem, opposite facing leaves, with jagged or serrated edges and a terminal flower. The species name Mentha comes from Roman Mythology. Minthe was a lovely young nymph who caught the eye of Pluto, the ruler of the underworld. When Pluto\'\s wife Persephone found out about his love for the beautiful nymph, she was enraged. She changed Minthe into a lowly plant, to be trodden underfoot. Pluto couldn\'\t reverse Persephone\'\s curse, but he did soften the spell somewhat by making the smell that Minthe gave off all the sweeter when she was tread upon.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Guava - Powder (1oz)",
+    img: "img/product-7.1.1.png",
+    price: 30,
+    company: "Carolina & Caribbean Botanicals",
+    info: 'Guavas are tropical trees originating in Central America. Their fruits are oval in shape with light green or yellow skin and contain edible seeds. What\’\s more, guava leaves are used as an herbal tea and the leaf extract as a supplement. Guava fruits are amazingly rich in antioxidants, vitamin C, potassium, and fiber. This remarkable nutrient content gives them many health benefits.',
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Rosemary - Powder (1oz)",
+    img: "img/product-8.1.1.png",
+    price: 2,
+    company: "Carolina & Caribbean Botanicals",
+    info:
+      'Rosemary is a popular member of the mint family, native to the Mediterranean regions near modern Greece. Poets and physicians from antiquity through the present day have lauded Rosemary. The name derives from the Latin for “Dew” (ros) and “Sea” (marinus). It can indeed survive with just the sea mist and ambient humidity in its native range. It has been long associated with memory, youth, fertility, romance. In Christian Mythology, The Virgin Mary is said to have spread her blue cloak over a white-blossomed rosemary bush when she was resting, and the flowers turned blue. The shrub then became known as the \'\Rose of Mary\'\. The culinary uses are many and it was also used as a preservative for meat prior to refrigeration along with Oregano and Thyme.',
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
